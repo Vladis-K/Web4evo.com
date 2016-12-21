74cd7bbb32cb3e180b15f7f539eea39a95a7dbb6
Vladys <vladis.kh@gmail.com>
2016-12-21 19:41:27 +0200

    nav_fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
-	$('.parallax').parallax();
-    $(".button-collapse").sideNav();
 
+	$('.parallax').parallax();
+    $('.button-collapse').sideNav({
+        menuWidth: 300,
+        edge: 'left',
+        closeOnClick: true,
+        draggable: true
+    });
+    
     $(window).scroll(function () {
-        display = $(this).scrollTop();
-        opRange = 1 - display / 300;
+        var display = $(this).scrollTop();
+        var opRange = 1 - display / 300;
         $("nav").css("opacity" , opRange);
         if (opRange > 0 ) {
             $("nav").css("display" , 'inline');
         }
         else {
-            $("nav").css("display" , "none");
+           $("nav").css("display" , "none");
         }
     });
 
@@ -20,27 +26,30 @@ $(document).ready(function(){
             parPosition.push($(this).offset().top);
         });
 
-		$('a').click(function(){
-			$('html, body').animate({
-				scrollTop: $( $.attr(this, 'href') ).offset().top
-			}, 1200);
-			return false;
-		});
-        
+    $("nav, .dotted").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body, html').animate({
+            scrollTop: top
+        }, 1200);
+    });
+    
+
         	$('.dotted ul li a').click(function () {
 			$('.dotted ul li a').removeClass('active');
 				$(this).addClass('active');
-		}); 
-        
+		});
+
        $('.dotted a').hover(function() {
            $(this).find('.label').show();
            }, function() {
            $(this).find('.label').hide();
        });
-        
+
         $(document).scroll(function(){
         var position = $(document).scrollTop(),
-                index; 
+                index;
                 for (var i=0; i<parPosition.length; i++) {
                 if (position <= parPosition[i]) {
                     index = i;
@@ -50,11 +59,11 @@ $(document).ready(function(){
       $('.dotted ul li a').removeClass('active');
             $('.dotted ul li a:eq('+index+')').addClass('active');
         });
-        
+
         	$('.dotted ul li a').click(function () {
 			$('.dotted ul li a').removeClass('active');
             $(this).addClass('active');
-	});   
+	});
 });
 
 
